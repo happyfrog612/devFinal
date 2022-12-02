f37e77090890cf882d5832e0bb1f00bfd9e507f7
happyfrog612 <happyfrog612@users.noreply.github.com>
2022-12-02 21:08:01 +0000

    deploy: 2e9897a0a798f0735a92d8803ca553c2c215edc7

--- precache-manifest.5230222f5a218eb051f1aa9b77a3319d.js
+++ precache-manifest.5230222f5a218eb051f1aa9b77a3319d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "9619ff9d29dea2ccfb50",
+    "revision": "a251da9a254491e061d6",
     "url": "devFinal/static/css/main.260da8a5.chunk.css"
   },
   {
-    "revision": "9619ff9d29dea2ccfb50",
-    "url": "devFinal/static/js/main.9619ff9d.chunk.js"
+    "revision": "a251da9a254491e061d6",
+    "url": "devFinal/static/js/main.a251da9a.chunk.js"
   },
   {
     "revision": "ff178d15e45f02213175",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "devFinal/static/media/logo.06e73328.svg"
   },
   {
-    "revision": "c9d0f9c20e85f3b0a281e7273ffe3755",
+    "revision": "90e1d8f74bb55a637bae0f9bb2676e3a",
     "url": "devFinal/index.html"
   }
 ];
