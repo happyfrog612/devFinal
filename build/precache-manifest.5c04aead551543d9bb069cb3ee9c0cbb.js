83a43d82ff25fdb2d359b3a1bec36d0bd8ff9cc4
happyfrog612 <114898366+happyfrog612@users.noreply.github.com>
2022-12-02 15:26:33 -0500

    Add files via upload

---
+++ build/precache-manifest.5c04aead551543d9bb069cb3ee9c0cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09566164955de7e526ed",
+    "url": "/devFinal/static/css/main.b47eb3d0.chunk.css"
+  },
+  {
+    "revision": "09566164955de7e526ed",
+    "url": "/devFinal/static/js/main.09566164.chunk.js"
+  },
+  {
+    "revision": "ff178d15e45f02213175",
+    "url": "/devFinal/static/js/1.ff178d15.chunk.js"
+  },
+  {
+    "revision": "8f8e66ca2e1ef74eec1a",
+    "url": "/devFinal/static/js/runtime~main.8f8e66ca.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/devFinal/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "749ca46fe2e7b2a61431e477b84ca5a8",
+    "url": "/devFinal/index.html"
+  }
+];
